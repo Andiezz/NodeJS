79e259f7093a0b55d3747943e55bf3491312d68d
Vũ Thành An <superlegend27@gmail.com>
2022-10-11 10:06:30 +0700

    set up mongodb connection

--- controllers/admin.js
+++ controllers/admin.js
@@ -30,6 +30,7 @@ exports.postAddProduct = (req, res, next) => {
     });
 };
 
+/*
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if (!editMode) {
@@ -100,3 +101,4 @@ exports.postDeleteProduct = (req, res, next) => {
     })
     .catch(err => console.log(err));
 };
+*/
--- util/database.js
+++ util/database.js
@@ -1,8 +1,31 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb")
+const MongoClient = mongodb.MongoClient
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+    MongoClient.connect(
+        "mongodb+srv://AndiexPie6:JnoRVDRbvQXk4kPl@nodejs-learning.odff7nk.mongodb.net/?retryWrites=true&w=majority"
+    )
+        .then((client) => {
+            console.log("Connected!")
+            //? store connection to the db in _db
+            //? create new db if have not existed
+            _db = client.db("shop")
+            callback(client)
+        })
+        .catch((err) => {
+            console.log(err)
+            throw err
+        })
+}
+
+const getDb = () => {
+  if (_db) {
+    return _db
+  }
+  throw new Error("No database found!")
+}
+
+exports.mongoConnect = mongoConnect
+exports.getDb = getDb
