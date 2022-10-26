57ba930d2de9ffc27094b76928386179a5e13a76
Andiezz <superlegend27@gmail.com>
2022-10-26 16:59:03 +0700

    Session-Cookies #1

--- app.js
+++ app.js
@@ -3,27 +3,52 @@ const path = require("path")
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
+const session = require("express-session")
+const MongoDBStore = require("connect-mongodb-session")(session)
 
 const errorController = require("./controllers/error")
 // const mongoConnect = require("./util/database").mongoConnect
 const User = require("./models/user")
 
+const MONGODB_URI =
+    "mongodb+srv://AndiexPie6:JnoRVDRbvQXk4kPl@nodejs-learning.odff7nk.mongodb.net/shop?authSource=admin"
+
 const app = express()
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: "sessions",
+})
 
 app.set("view engine", "ejs")
 app.set("views", "views")
 
 const adminRoutes = require("./routes/admin")
 const shopRoutes = require("./routes/shop")
+const authRoutes = require("./routes/auth")
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, "public")))
+app.use(
+    session({
+        secret: "Grace's secret",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {},
+        store: store,
+    })
+)
 
 app.use((req, res, next) => {
-    User.findById("6354a0844b68b83cd2ae48d4")
+    if (!req.session.user) {
+        return next()
+    }
+    User.findById(req.session.user._id)
         .then((user) => {
-            // req.user = user //! not the real user
-            req.user = user //? mongoose model (can access to all methods)
+            // req.user = user //! not the real
+            // req.session.isLoggedIn = true //? set any key you want
+            // req.session.user = user //? mongoose does not recognize user model
+            // req.user = user //? mongoose model (can access to all methods)
+            req.user = user
             next()
         })
         .catch((err) => console.log(err))
@@ -31,13 +56,12 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes)
 app.use(shopRoutes)
+app.use(authRoutes)
 
 app.use(errorController.get404)
 
 mongoose
-    .connect(
-        "mongodb+srv://AndiexPie6:JnoRVDRbvQXk4kPl@nodejs-learning.odff7nk.mongodb.net/shop?authSource=admin"
-    )
+    .connect(MONGODB_URI)
     .then((result) => {
         User.findOne().then((user) => {
             if (!user) {
--- controllers/admin.js
+++ controllers/admin.js
@@ -7,6 +7,7 @@ exports.getAddProduct = (req, res, next) => {
         pageTitle: "Add Product",
         path: "/admin/add-product",
         editing: false,
+        isAuthenticated: req.session.isLoggedIn
     })
 }
 
@@ -51,6 +52,7 @@ exports.getEditProduct = (req, res, next) => {
                 path: "/admin/edit-product",
                 editing: editMode,
                 product: product,
+                isAuthenticated: req.session.isLoggedIn
             })
         })
         .catch((err) => console.log(err))
@@ -87,6 +89,7 @@ exports.getProducts = (req, res, next) => {
                 prods: products,
                 pageTitle: "Admin Products",
                 path: "/admin/products",
+                isAuthenticated: req.session.isLoggedIn
             })
         })
         .catch((err) => console.log(err))
