f3cdc350f5f4b90d692d40d09b2e239c1699ca4b
Vũ Thành An <superlegend27@gmail.com>
2022-10-29 09:24:15 +0700

    Revert "Session-Cookies #1"
    This reverts commit 57ba930d2de9ffc27094b76928386179a5e13a76.

--- app.js
+++ app.js
@@ -3,52 +3,27 @@ const path = require("path")
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-const session = require("express-session")
-const MongoDBStore = require("connect-mongodb-session")(session)
 
 const errorController = require("./controllers/error")
 // const mongoConnect = require("./util/database").mongoConnect
 const User = require("./models/user")
 
-const MONGODB_URI =
-    "mongodb+srv://AndiexPie6:JnoRVDRbvQXk4kPl@nodejs-learning.odff7nk.mongodb.net/shop?authSource=admin"
-
 const app = express()
-const store = new MongoDBStore({
-    uri: MONGODB_URI,
-    collection: "sessions",
-})
 
 app.set("view engine", "ejs")
 app.set("views", "views")
 
 const adminRoutes = require("./routes/admin")
 const shopRoutes = require("./routes/shop")
-const authRoutes = require("./routes/auth")
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, "public")))
-app.use(
-    session({
-        secret: "Grace's secret",
-        resave: false,
-        saveUninitialized: false,
-        cookie: {},
-        store: store,
-    })
-)
 
 app.use((req, res, next) => {
-    if (!req.session.user) {
-        return next()
-    }
-    User.findById(req.session.user._id)
+    User.findById("6354a0844b68b83cd2ae48d4")
         .then((user) => {
-            // req.user = user //! not the real
-            // req.session.isLoggedIn = true //? set any key you want
-            // req.session.user = user //? mongoose does not recognize user model
-            // req.user = user //? mongoose model (can access to all methods)
-            req.user = user
+            // req.user = user //! not the real user
+            req.user = user //? mongoose model (can access to all methods)
             next()
         })
         .catch((err) => console.log(err))
@@ -56,12 +31,13 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes)
 app.use(shopRoutes)
-app.use(authRoutes)
 
 app.use(errorController.get404)
 
 mongoose
-    .connect(MONGODB_URI)
+    .connect(
+        "mongodb+srv://AndiexPie6:JnoRVDRbvQXk4kPl@nodejs-learning.odff7nk.mongodb.net/shop?authSource=admin"
+    )
     .then((result) => {
         User.findOne().then((user) => {
             if (!user) {
--- controllers/shop.js
+++ controllers/shop.js
@@ -8,7 +8,6 @@ exports.getProducts = (req, res, next) => {
                 prods: products,
                 pageTitle: "All Products",
                 path: "/products",
-                isAuthenticated: req.session.isLoggedIn
             })
         })
         .catch((err) => {
@@ -24,7 +23,6 @@ exports.getProduct = (req, res, next) => {
                 product: product,
                 pageTitle: product.title,
                 path: "/products",
-                isAuthenticated: req.session.isLoggedIn
             })
         })
         .catch((err) => console.log(err))
@@ -37,7 +35,6 @@ exports.getIndex = (req, res, next) => {
                 prods: products,
                 pageTitle: "Shop",
                 path: "/",
-                isAuthenticated: req.session.isLoggedIn
             })
         })
         .catch((err) => {
@@ -55,7 +52,6 @@ exports.getCart = (req, res, next) => {
                 path: "/cart",
                 pageTitle: "Your Cart",
                 products: products,
-                isAuthenticated: req.session.isLoggedIn
             })
         })
         .catch((err) => console.log(err))
@@ -94,6 +90,7 @@ exports.postOrder = (req, res, next) => {
                     quantity: i.quantity,
                 }
             })
+            console.log(products)
             const order = new Order({
                 user: {
                     name: req.user.name,
@@ -119,7 +116,6 @@ exports.getOrders = (req, res, next) => {
                 path: "/orders",
                 pageTitle: "Your Orders",
                 orders: orders,
-                isAuthenticated: req.session.isLoggedIn
             })
         })
         .catch((err) => {
@@ -131,6 +127,5 @@ exports.getCheckout = (req, res, next) => {
     res.render("shop/checkout", {
         path: "/checkout",
         pageTitle: "Checkout",
-        isAuthenticated: req.session.isLoggedIn
     })
 }
