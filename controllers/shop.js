5cbe641c09081829326d2e37c1d86425ae6c2c0e
Andiezz <superlegend27@gmail.com>
2022-11-17 13:46:53 +0700

    working with files #4

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,8 @@
 const fs = require("fs")
 const path = require("path")
 
+const PDFDocument = require("pdfkit")
+
 const Product = require("../models/product")
 const Order = require("../models/order")
 
@@ -166,6 +168,31 @@ exports.getInvoice = (req, res, next) => {
             }
             const invoiceName = `invoice-${orderId}.pdf`
             const invoicePath = path.join("data", "invoices", invoiceName)
+
+            const pdfDoc = new PDFDocument()
+            res.setHeader("Content-Type", "application/pdf")
+            res.setHeader(
+                "Content-Disposition",
+                `attachment; filename="${invoiceName}"`
+            )
+            pdfDoc.pipe(fs.createWriteStream(invoicePath))
+            pdfDoc.pipe(res)
+
+            pdfDoc.fontSize(26).text("Invoice", {
+                underline: true,
+                textIndent: true
+            })
+            pdfDoc.text("------------------------")
+            let totalPrice = 0
+            order.products.forEach(prod => {
+                totalPrice += prod.quantity * prod.product.price
+                pdfDoc.fontSize(14).text(`${prod.product.title} - ${prod.quantity} x $${prod.product.price}`)
+            })
+            pdfDoc.text("------------------------")
+            pdfDoc.fontSize(20).text(`Total price: $${totalPrice}`)
+
+            pdfDoc.end()
+
             // fs.readFile(invoicePath, (err, data) => {
             //     if (err) {
             //         return next(err)
@@ -177,6 +204,15 @@ exports.getInvoice = (req, res, next) => {
             //     )
             //     res.send(data)
             // })
+            //? STREAMING FILE
+            // const file = fs.createReadStream(invoicePath)
+            // res.setHeader("Content-Type", "application/pdf")
+            // res.setHeader(
+            //     "Content-Disposition",
+            //     `attachment; filename="${invoiceName}"`
+            // )
+            // //? forward the read file to response (writable stream)
+            // file.pipe(res)
         })
         .catch((err) => {
             next(err)
