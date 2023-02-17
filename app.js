72a44e2e225faca56aee3ff908c740fe27e1c704
Vũ Thành An <superlegend27@gmail.com>
2023-02-17 10:45:37 +0700

    prepared for deployment

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ const store = new MongoDBStore({
 })
 const csrfProtection = csrf()
 
-const privateKey = fs.readFileSync("server.key")
-const certificate = fs.readFileSync("server.cert")
+// const privateKey = fs.readFileSync("server.key")
+// const certificate = fs.readFileSync("server.cert")
 
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -133,8 +133,9 @@ app.use((error, req, res, next) => {
 mongoose
     .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => {
-        https
-            .createServer({ key: privateKey, cert: certificate }, app)
-            .listen(process.env.PORT || 3000)
+        // https
+        //     .createServer({ key: privateKey, cert: certificate }, app)
+        //     .listen(process.env.PORT || 3000)
+        app.listen(process.env.PORT || 3000)
     })
     .catch((err) => console.log(err))
